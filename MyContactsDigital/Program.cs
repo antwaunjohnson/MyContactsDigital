342254d9e2faa32af741faf4dbbdeec40a123701
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-09-08 20:44:09 -0700

    updated application to use postgresql instead of sqlserver as the database, applied initial migrations

--- MyContactsDigital/Program.cs
+++ MyContactsDigital/Program.cs
@@ -5,9 +5,10 @@ using MyContactsDigital.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
