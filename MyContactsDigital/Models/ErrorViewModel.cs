cd6f3d9c82fce1d8a1f9f91faba0298685e6e74f
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-09-08 20:02:40 -0700

    Add project files.

---
+++ MyContactsDigital/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyContactsDigital.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
