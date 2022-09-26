b8077271faf82fc633cc04b7301b7cbd0ee4c755
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-09-26 14:22:53 -0700

    created and registered an EmailService

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using MyContactsDigital.Data;
 using MyContactsDigital.Models;
@@ -22,6 +23,9 @@ builder.Services.AddControllersWithViews();
 ///custom services
 builder.Services.AddScoped<IImageService, ImageService>();
 builder.Services.AddScoped<IAddressBookService, AddressBookService>();
+builder.Services.AddScoped<IEmailSender, EmailService>();
+
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
 var app = builder.Build();
 
