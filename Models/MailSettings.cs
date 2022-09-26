2e0ee01ea2cd6e21856f0ec551344a80fd88da65
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-09-26 13:43:10 -0700

    created MailSettings model

---
+++ Models/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace MyContactsDigital.Models;
+
+public class MailSettings
+{
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? DisplayName { get; set; }
+    public string? Host { get; set; }
+    public int Port { get; set; }
+
+}
