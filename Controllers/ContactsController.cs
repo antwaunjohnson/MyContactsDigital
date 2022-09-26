f4c1d2ab1d8110cd69e8c1a0b17f85820fc527d0
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-09-26 13:15:49 -0700

    Created models for EmailContact and styled the EmailContact View

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -11,6 +11,7 @@ using MyContactsDigital.Data;
 using MyContactsDigital.Models;
 using MyContactsDigital.Enums;
 using MyContactsDigital.Services.Interfaces;
+using MyContactsDigital.Models.ViewModels;
 
 namespace MyContactsDigital.Controllers
 {
@@ -94,9 +95,30 @@ namespace MyContactsDigital.Controllers
         }
 
         [Authorize]
-        public IActionResult EmailContact(int ContactId)
+        public async Task<IActionResult> EmailContact(int id)
         {
-            return View();
+            string appUserId = _userManager.GetUserId(User);
+            Contact? contact = await _context.Contacts.Where(c => c.Id == id && c.AppUserId == appUserId).FirstOrDefaultAsync();
+
+            if(contact == null)
+            {
+                return NotFound();
+            }
+
+            EmailData emailData = new EmailData()
+            {
+                EmailAddress = contact.Email,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+            };
+
+            EmailContactViewModel model = new EmailContactViewModel()
+            {
+                Contact = contact,
+                EmailData = emailData
+            };
+
+            return View(model);
         }
 
         // GET: Contacts/Details/5
---
+++ Models/ViewModels/EmailContactViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MyContactsDigital.Models.ViewModels;
+
+public class EmailContactViewModel
+{
+    public Contact? Contact { get; set; }
+    public EmailData? EmailData { get; set; }
+}
