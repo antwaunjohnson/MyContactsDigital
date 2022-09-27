0bb46f6d8b6b3e57db3093ecc4f644d9a26b3e2a
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-09-27 08:24:29 -0700

    Styled EmailCateogy view, created viewmodel, injected emailService into CategoriesController implemented corresponding actions in the controller to send email to all email addresses in a category

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using MyContactsDigital.Data;
 using MyContactsDigital.Models;
 using MyContactsDigital.Models.ViewModels;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace MyContactsDigital.Controllers
 {
@@ -17,17 +18,20 @@ namespace MyContactsDigital.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IEmailSender _emailService;
 
-        public CategoriesController(ApplicationDbContext context, UserManager<AppUser> userManager)
+        public CategoriesController(ApplicationDbContext context, UserManager<AppUser> userManager, IEmailSender emailService)
         {
             _context = context;
             _userManager = userManager;
+            _emailService = emailService;
         }
 
         // GET: Categories
         [Authorize]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string swalMessage = null)
         {
+            ViewData["SwalMessage"] = swalMessage;
             string appUserId = _userManager.GetUserId(User);
 
 
@@ -36,6 +40,53 @@ namespace MyContactsDigital.Controllers
                 .Include(c => c.AppUser)
                 .ToListAsync();
             return View(categories);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> EmailCategory(int id)
+        {
+            string appUserId = _userManager.GetUserId(User);
+            Category? category = await _context.Categories
+                .Include(c => c.Contacts)
+                .FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
+            List<string> emails = category.Contacts.Select(c => c.Email).ToList();
+
+            EmailData emailData = new EmailData()
+            {
+                GroupName = category.Name,
+                EmailAddress = String.Join(";", emails),
+                Subject = $"Group Message: {category.Name}"
+
+            };
+
+            EmailCategoryViewModel model = new EmailCategoryViewModel()
+            {
+                Contacts = category.Contacts.ToList(),
+                EmailData = emailData
+            };
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EmailCategory(EmailCategoryViewModel ecvm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _emailService.SendEmailAsync(ecvm.EmailData.EmailAddress, ecvm.EmailData.Subject, ecvm.EmailData.Body);
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Success: Emal Sent!" });
+                }
+                catch
+                {
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Error: Emal Send Failed!" });
+                    throw;
+                }
+            }
+
+            return View(ecvm);
         }
 
         // GET: Categories/Details/5
---
+++ Models/ViewModels/EmailCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyContactsDigital.Models.ViewModels;
+
+public class EmailCategoryViewModel
+{
+    public List<Contact>? Contacts { get; set; }
+
+    public EmailData? EmailData { get; set; }
+}
